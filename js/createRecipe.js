0122221b3fbbeb2dc7c511e4737ae14857dc7460
Judah Bennett <judah.bennett12@gmail.com>
2018-10-08 19:15:24 +1000

    createRecipe js, addingredients/steps, deleteingredients/steps, turn form values into json file

---
+++ js/createRecipe.js
@@ -0,0 +1,21 @@
+function checkRecipe(){
+    var recipeTitle = $('#recipeTitle').val();
+    var recipeType = $('#recipeType').val();
+    var hour = $('#hour').val();
+    var mintues = $('#mintues').val();
+    var recipeLevel = $('#recipeLevel').val();
+    var cost = $('#cost').val();
+}
+
+function addIngredients(){
+
+}
+function deleteStep(){
+    
+}
+function addStep(){
+    
+}
+function deleteStep(){
+    
+}
