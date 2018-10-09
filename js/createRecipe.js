31794b7341bbee10d52b3d51af617ebc9bff0c91
Judah Bennett <judah.bennett12@gmail.com>
2018-10-09 11:58:15 +1000

    Add and Delete ingredient added, not tested

--- js/createRecipe.js
+++ js/createRecipe.js
@@ -1,3 +1,8 @@
+//to give each individual ingredient and step an individual id
+var ingredientCount = 1;
+var stepCount = 1;
+
+//check fields aren't empty
 function checkRecipe(){
     var recipeTitle = $('#recipeTitle').val();
     var recipeType = $('#recipeType').val();
@@ -7,10 +12,33 @@ function checkRecipe(){
     var cost = $('#cost').val();
 }
 
-function addIngredients(){
-
+function jsonRecipe(){
+    
 }
-function deleteStep(){
+
+function addIngredient(){
+    $("#ingredientList").append(
+        '<tr>' +
+                                '<td>' +
+                                    '<input class="form-control" name="name[]" type="text" id="ingredient"' + ingredientCount + placeholder="Ingredient Name"/>'+
+                                '</td>'+
+                                '<td>'+
+                                    '<input class="form-control" name="amount[]" type="text" id="amount"' + ingredientCount + 'placeholder="Amount"/>'+
+                                '</td>'+
+                                '<td>'+
+                                    '<button class="btn btn-danger btn-remove" type="button" onclick="deleteIngredient(this)">'+
+                                    '<i class="fa fa-minus gs mr-1">' + '</i>' + 'Delete'+
+                                    '</button>'+
+                                '</td>'+
+                            '</tr>'
+    );
+    ingredientCount++;
+}
+function deleteIngredient(item){
+    var td = item.parentElement;
+    var tr = td.parentElement;
+    tr.parentElement.removeChild(tr);
+
     
 }
 function addStep(){
