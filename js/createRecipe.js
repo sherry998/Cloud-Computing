27e4a1e37cb01604590e84e9b2151edb4b05f802
Judah Bennett <judah.bennett12@gmail.com>
2018-10-09 12:35:36 +1000

    able to add steps and delete steps, for some reason clicking add for ingredients makes two ingredients

--- js/createRecipe.js
+++ js/createRecipe.js
@@ -1,5 +1,5 @@
 //to give each individual ingredient and step an individual id
-var ingredientCount = 1;
+var ingredientCount = 2;
 var stepCount = 1;
 
 //check fields aren't empty
@@ -20,10 +20,10 @@ function addIngredient(){
     $("#ingredientList").append(
         '<tr>' +
                                 '<td>' +
-                                    '<input class="form-control" name="name[]" type="text" id="ingredient"' + ingredientCount + placeholder="Ingredient Name"/>'+
+                                    '<input class="form-control" name="name[]" type="text" id="ingredient' + ingredientCount + '" placeholder="Ingredient Name"/>'+
                                 '</td>'+
                                 '<td>'+
-                                    '<input class="form-control" name="amount[]" type="text" id="amount"' + ingredientCount + 'placeholder="Amount"/>'+
+                                    '<input class="form-control" name="amount[]" type="text" id="amount' + ingredientCount + '" placeholder="Amount"/>'+
                                 '</td>'+
                                 '<td>'+
                                     '<button class="btn btn-danger btn-remove" type="button" onclick="deleteIngredient(this)">'+
@@ -34,16 +34,47 @@ function addIngredient(){
     );
     ingredientCount++;
 }
+
 function deleteIngredient(item){
     var td = item.parentElement;
     var tr = td.parentElement;
     tr.parentElement.removeChild(tr);
-
+    ingredientCount--;
     
 }
 function addStep(){
-    
+    $("#stepContainer").insertBefore(
+        '<div id="step'+ stepCount + '" class="form-group">'+
+            '<h3><b>Step' + stepCount+ '</b></h3>' +
+                '<div class="form-inline">' +
+                    '<button type="button" name="deleteStep" class=" delete btn btn-danger" onclick="deleteStep(this)">' +
+                       'Delete Step'+
+                    '</button>'+
+
+                    '<label for="image-upload" class="create-btn btn btn-primary mb-0" style="margin-left: 2%;">'+
+                            'Add Step image'+
+                    '</label>'+
+                    '<input id="image-upload" name="image-upload1" type="file" style="display:none;" accept="image/x-png,image/gif,image/jpeg" multiple/>'+
+
+                '</div>'+
+                '<br>'+
+                '<div class="form-group">'+
+                    '<textarea name="content1" cols="88" rows="8" placeholder="Write Your step Detail here..." class="form-control"></textarea>'+
+                '</div>'+
+                '<div id="preview' + stepCount + '" class="imagePreview form-group">'+
+
+                '</div>'+
+        '</div>'
+        
+        , '#stepContainer.childNodes[#stepContainer.length]'
+
+    );
+    stepCount++;
 }
-function deleteStep(){
+function deleteStep(item){
+    var td = item.parentElement;
+    var tr = td.parentElement;
+    tr.parentElement.removeChild(tr);
+    ingredientCount--;
     
 }
